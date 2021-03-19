d2c46a47aca5194240965d270080a26aa67943c6
qer <wbxl2000@outlook.com>
2021-03-19 18:24:02 +0800

    feat(strokes): update new dataset & more reasonable loading method

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../front-end/build')));
-app.use(express.static(path.join(__dirname, './dataset')));
+app.use(express.static(path.join(__dirname, './dataSet_100per')));
 
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -24,11 +24,13 @@ app.get('/', function(req, res) {
 });
 
 app.get('/api/system-info', (req, res) => {
+  console.log('/api/system-info');
   const systemInfo = JSON.parse(fs.readFileSync(__dirname + '\\src\\config\\system-info.json'));
   res.json(systemInfo);
 });
 
 app.get('/api/strokes-list', (req, res) => {
+  console.log('/api/strokes-list');
   const { characters } = JSON.parse(fs.readFileSync(__dirname + '\\src\\config\\strokes-list.json'));
   if (!characters) res.end();
   const { id } = req.query;
@@ -40,21 +42,51 @@ app.get('/api/strokes-list', (req, res) => {
 
 
 app.get('/api/character-info', (req, res) => {
+  console.log('/api/character-info');
   const { id } = req.query;
-  const { images } = JSON.parse(fs.readFileSync(__dirname + '\\src\\config\\images.json'));
-  const img = images.find((item) => {
+  const { dataSetId } = JSON.parse(fs.readFileSync(`${__dirname}\\src\\config\\system-info.json`));
+  const images = JSON.parse(fs.readFileSync(__dirname + `\\dataSet_100per\\${dataSetId}\\images.json`));
+  const img = images.images.find((item) => {
     return (item.id.toString() === id)
   });
-  res.json(img);
+  const imgJson = {
+    total: images.sum,
+    img
+  }
+  res.json(imgJson);
+});
+
+app.post('/api/next-dataset', (req, res) => {
+  console.log('/api/next-dataset');
+  fs.readFile(`${__dirname}\\src\\config\\system-info.json`, (err, data) => {  // READ
+    if (err) {
+        res.end("error" + err);
+        return console.error(err);
+    };
+    const newData = JSON.parse(data.toString());
+    newData.dataSetId = req.body.dataSetId + 1;
+    newData.currentImageId = 1;
+    newData.author = req.body.author;
+    const writeData = fs.writeFile(`${__dirname}\\src\\config\\system-info.json`, JSON.stringify(newData), (err, result) => {  // WRITE
+        if (err) {
+          res.end("error"+ err);
+          return console.error(err);
+        } else {
+          res.end("success");
+        }
+    });
+  });
 });
 
 
 
+
 app.post('/api/submit', (req, res) => {
+  console.log('/api/submit');
   const nextImage = () => {
     fs.readFile(`${__dirname}\\src\\config\\system-info.json`, (err, data) => {  // READ
       if (err) {
-          res.end("error"+ err);
+          res.end("error" + err);
           return console.error(err);
       };
       const newData = JSON.parse(data.toString());
@@ -70,14 +102,12 @@ app.post('/api/submit', (req, res) => {
       });
     });    
   }
-
-  
   fs.readFile(`${__dirname}\\result\\data.json`, (err, data) => {  // READ
     if (err) {
         res.end("error"+ err);
         return console.error(err);
     };
-    console.log(data);
+    // console.log(data);
     const newData = JSON.parse(data.toString());
     newData.content.push(req.body);
     const writeData = fs.writeFile(`${__dirname}\\result\\data.json`, JSON.stringify(newData), (err, result) => {  // WRITE
